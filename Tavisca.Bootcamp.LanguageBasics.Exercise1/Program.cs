a44300e46e869415e1f9cd04ab02c24da79d50ff
Neelesh Rajpurohit <nrajpurohit@tavisca.in>
2019-06-28 14:55:50 +0530

    Updated code with changes as suggested by Mentor.

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -20,154 +21,125 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"{args} : {result}");
         }
 
-        public static int FindDigit(string equation)
+        public class Operand
         {
-            /*
+            String[] num = new String[3]; // stores the operand strings
+            int[] number = new int[3]; // stores operand integers
+            int questionMarkIndex= -1; // to store which operand has the missing digit.
+            int validNumber; // This will be our number that makes the equation valid.
+            String missingOperand;
+            String validOperand;
+            int result = 0;
+            int notDivisible =0;
+
+            public void ExtractNum(string equation){
+                
+                Regex pattern = new Regex(@"(?<num0>.*)\*(?<num1>.*)=(?<num2>.*)");
+                Match match = pattern.Match(equation);
+                this.num[0] = match.Groups["num0"].Value;
+                this.num[1] = match.Groups["num1"].Value;
+                this.num[2] = match.Groups["num2"].Value;
+                
+                this.notDivisible =0; // initialization for each testcase.                
+            }
+
+            public void FindQuestionMark()
+            {
+                if(this.num[0].Contains('?'))
+                {
+                    this.questionMarkIndex = 0;
+                }else if(this.num[1].Contains('?'))
+                {
+                    this.questionMarkIndex = 1;
+                }else if(this.num[2].Contains('?'))
+                {
+                    this.questionMarkIndex = 2;
+                }
+                
+            }
+
+            public void InitializeNumbers()
+            {
+                this.missingOperand = this.num[this.questionMarkIndex];
+                if(this.questionMarkIndex==0)
+                {
+                    if(Int32.TryParse(this.num[1],out this.number[1]))
+                        this.result = -1;
+                    if(Int32.TryParse(this.num[2],out this.number[2]))
+                        this.result = -1;
+                    if(this.number[2] % this.number[1] != 0)
+                    {
+                        this.notDivisible = 1;
+                    }
+                    this.number[0] = this.number[2] / this.number[1];
+                }else if(this.questionMarkIndex==1)
+                {
+                    if(Int32.TryParse(this.num[0],out this.number[0]))
+                        this.result = -1;
+                    if(Int32.TryParse(this.num[2],out this.number[2]))
+                        this.result = -1;
+                    if(this.number[2] % this.number[0] != 0)
+                    {
+                        this.notDivisible = 1;
+                    }
+                    this.number[1] = this.number[2] / this.number[0];
+                }else if(this.questionMarkIndex==2)
+                {
+                    if(Int32.TryParse(this.num[0],out this.number[0]))
+                        this.result = -1;
+                    if(Int32.TryParse(this.num[1],out this.number[1]))
+                        this.result = -1;
+                    this.number[2] = this.number[0] * this.number[1]; 
+                }
+
+                this.validNumber = this.number[this.questionMarkIndex];     // Valid number in integer
+                this.validOperand = this.number[this.questionMarkIndex].ToString();  // Valid operand string
+
+            }
+
+
+            public int FindMissingDigit()
+            {
+                char[] newone = this.validOperand.ToCharArray();
+
+                // let's replace ? with the actual result.
+                int index = this.missingOperand.IndexOf('?');
+                
+                    char missingDigit = newone[index];
+                    missingOperand = this.missingOperand.Replace('?',missingDigit);
+                    // Compare the strings for equality, id true then the missingdigit is the result to be returned
+                    if(String.Equals(this.missingOperand,this.validOperand)==true && this.notDivisible==0)
+                    {
+                        this.result= (int)Char.GetNumericValue(missingDigit);
+                    }else
+                    {
+                        this.result = -1;
+                    }
+                return this.result;
+            }
+
+        }
+
+        /*
             I have solved the problem in the following manner :
             1) extract the operands
             2) find which one has the missing digit
             3) find the actual number that would make the equation work as expected
             4) replace the missing digit by comparing the string to actual result
             5) return the missing digit if the result matches the operand after replacing the missing digit
-             */
+        */
 
+        public static int FindDigit(string equation)
+        {
+            var operand = new Operand();
 
-            // extracting the numbers out of the equation.
-            string[] num = new String[3];
-            int[] number = new int[3];
-            int questionMarkIndex= -1; // to store which operand has the missing digit.
-            int index=0;
-            int result=-1; // to store the character that is to be replaced by ?
-            string temp; // to store the actual retrieved number that has the 'question mark'
+            // extracting the numbers out of the equation. and finding the operand with missing digit
+            operand.ExtractNum(equation);
+            operand.FindQuestionMark();
+            operand.InitializeNumbers();
+            int result = operand.FindMissingDigit();
+            return result;
 
-            for(int i=0; i<equation.Length; i++){
-                if(equation[i]=='*'||equation[i]=='='){
-                    index++;
-                }else{
-                    num[index] += equation[i];
-                }
-            }
-            
-            // now let's find question mark index, to check which number has tha missing digit!
-            // and store the information in the variable questionMarkIndex
-            if(num[0].Contains('?')){
-                questionMarkIndex = 0;
-            }else if(num[1].Contains('?')){
-                questionMarkIndex = 1;
-            }else if(num[2].Contains('?')){
-                questionMarkIndex = 2;
-            }
-
-            if(questionMarkIndex==0){
-                //Console.WriteLine("missing number is First");
-                // Now we retieve the number missing!
-                number[1]= Int32.Parse(num[1]);
-                number[2]= Int32.Parse(num[2]);
-
-                number[0] = number[2]%number[1];
-                if(number[0]!=0){
-                    return -1;
-                }else
-                {
-                    number[0] = number[2]/number[1];
-                }
-                temp = number[0].ToString();
-
-                char[] temporary = temp.ToCharArray();
-                char[] orignal = num[0].ToCharArray();
-            
-                // let's replace ? with the actual result.
-                for(int i=0; i<orignal.Length; i++){
-                    if(orignal[i]=='?'){
-                        orignal[i]=temporary[i];
-                        result= (int)Char.GetNumericValue(temporary[i]); // storing the missing digit in result
-                    }
-                }
-                
-
-                //now let's compare the numbers orignal and temporary
-                num[0] = new String(orignal);
-                temp = new String(temporary);
-                if(String.Equals(num[0],temp)){
-                    return result; // returning the missing digit
-                }else {
-                    return -1;
-                }
-                // Process finished for FIRST operand missing ?
-
-            }else if(questionMarkIndex==1){
-                //Console.WriteLine("missing number is Second");
-                // Now we retieve the number missing!
-                number[0]= Int32.Parse(num[0]);
-                number[2]= Int32.Parse(num[2]);
-
-                number[1] = number[2]%number[0];
-                if(number[1]!=0){
-                    return -1;
-                }else
-                {
-                    number[1] = number[2]/number[0];
-                }
-                temp = number[1].ToString();
-
-                char[] temporary = temp.ToCharArray();
-                char[] orignal = num[1].ToCharArray();
-            
-                // let's replace ? with the actual result.
-                for(int i=0; i<orignal.Length; i++){
-                    if(orignal[i]=='?'){
-                        orignal[i]=temporary[i];
-                        result= (int)Char.GetNumericValue(temporary[i]); // storing the missing digit in result
-                    }
-                }
-                
-
-                //now let's compare the numbers orignal and temporary
-                num[1] = new String(orignal);
-                temp = new String(temporary);
-                if(String.Equals(num[1],temp)){
-                    return result; // returning the missing digit
-                }else {
-                    return -1;
-                }
-                // Process finished for SECOND operand missing ?
-
-            }else if(questionMarkIndex==2){
-                //Console.WriteLine("missing number is Third");
-                // Now we retieve the number missing!
-                number[1]= Int32.Parse(num[1]);
-                number[0]= Int32.Parse(num[0]);
-
-                number[2] = number[0]*number[1]; // multiplication to get actual result
-                
-                temp = number[2].ToString();
-
-                char[] temporary = temp.ToCharArray();
-                char[] orignal = num[2].ToCharArray();
-            
-                // let's replace ? with the actual result.
-                for(int i=0; i<orignal.Length; i++){
-                    if(orignal[i]=='?'){
-                        orignal[i]=temporary[i];
-                        result= (int)Char.GetNumericValue(temporary[i]); // storing the missing digit in result
-                    }
-                }
-                
-
-                //now let's compare the numbers orignal and temporary
-                num[2] = new String(orignal);
-                temp = new String(temporary);
-                if(String.Equals(num[2],temp)){
-                    return result; // returning the missing digit
-                }else {
-                    return -1;
-                }
-                // Process finished for first operand missing ?
-
-            }
-
-            return 1;
-            throw new NotImplementedException();
         }
     }
 }
